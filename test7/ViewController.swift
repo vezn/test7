6e0ddea40499a576f5c2307ad0b846c667a455de
vezn <espipers@gmail.com>
2019-01-09 01:32:33 +0200

    f edit

---
+++ test7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  test7
+//
+//  Created by Andrey Kedr on 09/01/2019.
+//  Copyright © 2019 Andrey Kedr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
